a1e4bba51fcfe0e0957e38a18698251be79399ca
Corentin Havet <Corentin.havet@ensimag.imag.fr>
2015-04-28 08:31:37 +0200

    Modification de la syntaxe
    Insertion de date fonctionnelle

--- inserts.sql
+++ inserts.sql
@@ -1,2 +1,7 @@
+
 INSERT INTO Adresse_Postale
 Values (18,'rue des marseillais',13000,'Marseille');
+
+
+INSERT INTO Personne
+Values (1,'Dupont','Gerard',to_date('1991-04-02','yyyy-mm-dd'),0606060606,'gege@cegetel.net',18,'rue des marseillais',13000,'Marseille');
